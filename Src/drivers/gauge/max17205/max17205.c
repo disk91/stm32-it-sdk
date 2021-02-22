2e8df51fe410faf94f81ec12f3411c1b43abae80
disk91 <disk_91@homtail.com>
2021-02-22 21:06:23 +0100

    Improve capacity management and fix coulomb delta calculation

--- Src/drivers/gauge/max17205/max17205.c
+++ Src/drivers/gauge/max17205/max17205.c
@@ -96,6 +96,19 @@ static gpio_irq_chain_t __max17205_gpio_irq = {
 		NULL,
 };
 
+/**
+ * Function to detect if the switch from value A to B causes an overflow.
+ * Return 1 if overflow, 0 otherwise.
+ */
+static uint8_t __overflow_detected(uint16_t p_u16A, uint16_t p_u16B)
+{
+	uint8_t l_u8Overflow = 0u;
+	if (p_u16A > (UINT16_MAX - p_u16B))	{
+		l_u8Overflow = 1u;
+	}
+	return l_u8Overflow;
+}
+
 
 /**
  * Setup the sensor according to the selected mode.
@@ -294,6 +307,23 @@ drivers_max17205_ret_e drivers_max17205_setup(drivers_max17205_mode_e mode) {
 		break;
 
 	}
+
+	#if defined ITSDK_DRIVERS_MAX17205_CAPA_MAX && ITSDK_DRIVERS_MAX17205_CAPA_MAX > 0
+	 // Configure design capacity
+	 // Check if DesignCap bit is enable
+	 __readRegister(ITSDK_DRIVERS_MAX17205_REG_NVCFG, &v);
+	 if ((v & ITSDK_DRIVERS_MAX17205_REG_NVCFG_DCAP_MSK) == 0) {
+	   v |= ITSDK_DRIVERS_MAX17205_REG_NVCFG_DCAP_MSK;
+	   __writeRegister(ITSDK_DRIVERS_MAX17205_REG_NVCFG, v);
+	 }
+	 // Set design capacity value
+	 // Maximum value that can be stored is 65 535 * LSB mAh
+	 // LSB = 5 / ITSDK_DRIVERS_MAX17205_RSENSE_MOHM
+	  v = (uint16_t) (ITSDK_DRIVERS_MAX17205_CAPA_MAX * (ITSDK_DRIVERS_MAX17205_RSENSE_MOHM/5));
+	  __writeRegister(ITSDK_DRIVERS_MAX17205_REG_NDCAP, v);
+	  __writeRegister(ITSDK_DRIVERS_MAX17205_REG_DCAP, v);
+	#endif
+
 	__max17205_config.initialized = MAX17205_SUCCESS;
 	return MAX17205_SUCCESS;
 }
@@ -406,9 +436,15 @@ drivers_max17205_ret_e drivers_max17205_getCoulomb(uint32_t * coulomb) {
 	uint16_t capa;
 	drivers_max17205_getCapacity(&capa);
 #if ITSDK_DRIVERS_MAX17205_CSN_TO_BAT == __ENABLE
+	if ((capa >= 0xFFFF) || ((capa < __max17205_config.lastCapa) && (__overflow_detected(__max17205_config.lastCapa,capa) == 0u))) {
+		return MAX17205_FAILED;
+	}
 	uint16_t delta = (capa >= __max17205_config.lastCapa )? (capa - __max17205_config.lastCapa) : (capa + (0xFFFF - __max17205_config.lastCapa));
 #else
-	uint16_t delta = (capa < __max17205_config.lastCapa )? __max17205_config.lastCapa - capa : (__max17205_config.lastCapa + (0xFFFF - capa));
+	if ((capa >= 0xFFFF) || ((capa > __max17205_config.lastCapa) && (__overflow_detected(capa,__max17205_config.lastCapa) == 0u))){
+		return MAX17205_FAILED;
+	}
+	uint16_t delta = (capa <= __max17205_config.lastCapa )? __max17205_config.lastCapa - capa : (__max17205_config.lastCapa + (0xFFFF - capa));
 #endif
 	__max17205_config.totalCapa += delta;
 	__max17205_config.lastCapa = capa;
